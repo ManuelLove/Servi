9418ca05014d34747d5ac9293f938d58ddd7d937
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-19 05:28:19 -0500

    Add files via upload

--- descarga-playlist.js
+++ descarga-playlist.js
@@ -2,7 +2,8 @@ import { generateWAMessageContent, generateWAMessageFromContent, proto } from '@
 import yts from 'yt-search';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-  if (!text) return m.reply(`Ejemplo : ${usedPrefix + command} historia de anime`);
+  if (!text) return m.reply(`𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+m.react('📀');
 
   try {
     let search = await yts(text);
