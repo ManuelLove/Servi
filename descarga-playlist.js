d9474a43a658e7a69c99e4af86531cac9942ef91
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-19 05:57:28 -0500

    Add files via upload

--- descarga-playlist.js
+++ descarga-playlist.js
@@ -1,16 +1,13 @@
 import { generateWAMessageContent, generateWAMessageFromContent, proto } from '@whiskeysockets/baileys';
 import yts from 'yt-search';
-if (!m.chat || typeof m.chat !== "string") {
-    console.error("⚠️ Error: `m.chat` no es un string válido.");
-    return;
-}
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-  if (!text) return m.reply(`${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m);
+  if (!text) return m.reply(`Ejemplo : ${usedPrefix + command} historia de anime`); fkontak,  m)
+m.react('📀');
 
   try {
-  m.react('📀');
     let search = await yts(text);
-  if (!text) return m.reply(`${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m);
+    if (!search.all.length) return m.reply("¡No se encontraron resultados de búsqueda!");
 
     // Enviar mensaje de búsqueda
     m.reply("🔍 Buscando en YouTube, por favor espera...");
