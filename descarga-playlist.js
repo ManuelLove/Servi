1a90c3f58dc6ebb4c544a9c8c7ba9907125b983b
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-19 05:15:03 -0500

    Add files via upload

--- descarga-playlist.js
+++ descarga-playlist.js
@@ -1,21 +1,16 @@
+
 import yts from 'yt-search';
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
-try {
-case 'ytbuscar': {
-  updatePopularCommand(command); // Mencatat command
-  if (!text) return m.reply(`Ejemplo : ${prefix + command} historia de anime`);
-  if (!firely(m, '⏳ Tratamiento..')) return; // Jika limit habis, proses berhenti di sini
+  if (!text) return m.reply(`Ejemplo : ${usedPrefix + command} historia de anime`);
 
   try {
-    // Cari hasil di YouTube menggunakan API
     let search = await yts(text);
     if (!search.all.length) return m.reply("¡No se encontraron resultados de búsqueda!");
 
-    // Enviar un mensaje de depuración antes de procesar la respuesta
+    // Enviar mensaje de búsqueda
     m.reply("🔍 Buscando en YouTube, por favor espera...");
 
-    // Batasi hasil pencarian ke 5 item teratas dan siapkan carousel card
     const carouselCards = await Promise.all(search.all.slice(0, 5).map(async (video, index) => ({
       header: {
         title: `Resultados ${index + 1}`,
@@ -25,8 +20,12 @@ case 'ytbuscar': {
         }, { upload: conn.waUploadToServer })).imageMessage
       },
       body: {
-        text: `🎥 *${video.title}*\n👁 *Vistas:* ${video.views}\n⏱ *Duración:* ${video.timestamp}\n📆 *Subido:* ${video.ago}\n📝 *Url:* ${video.url}`
-            },
+        text: `🎥 *${video.title}*
+👁 *Vistas:* ${video.views}
+⏱ *Duración:* ${video.timestamp}
+📆 *Subido:* ${video.ago}
+📝 *Url:* ${video.url}`
+      },
       footer: {
         text: `Haga clic en el botón a continuación para ver o copiar el enlace.`
       },
@@ -35,22 +34,22 @@ case 'ytbuscar': {
           {
             "name": "cta_copy",
             "buttonParamsJson": JSON.stringify({
-            "display_text": "🎵MUSICA🎵",
-            "copy_code": `${prefix}ytmp3 ${video.url}`
+              "display_text": "🎵MUSICA🎵",
+              "copy_code": `${usedPrefix}ytmp3 ${video.url}`
             })
           },
           {
             "name": "cta_copy",
             "buttonParamsJson": JSON.stringify({
-            "display_text": "📺VIDEO📺",
-            "copy_code": `${prefix}ytmp4 ${video.url}`
+              "display_text": "📺VIDEO📺",
+              "copy_code": `${usedPrefix}ytmp4 ${video.url}`
             })
           }
         ]
       }
     })));
 
-    // Buat pesan carousel
+    // Crear y enviar mensaje tipo carrusel
     const carouselMessage = generateWAMessageFromContent(m.chat, {
       viewOnceMessage: {
         message: {
@@ -63,7 +62,7 @@ case 'ytbuscar': {
               text: `🔎 *Resultados de búsqueda de YouTube para:* _${text}_`
             },
             footer: {
-              text: `Bot de YouTube de Techfix Solutions`
+              text: `Bot de YouTube`
             },
             header: {
               hasMediaAttachment: false
@@ -76,17 +75,22 @@ case 'ytbuscar': {
       }
     }, {});
 
-    // Kirim pesan carousel
     await conn.relayMessage(m.chat, carouselMessage.message, {
       messageId: carouselMessage.key.id
     });
 
   } catch (e) {
-    console.error("Error al procesar la solicitud de búsqueda de YouTube:", e);
+    console.error("Error al procesar la búsqueda de YouTube:", e);
     await conn.sendMessage(m.chat, {
-      text: "❌ Se produjo un error al procesar una búsqueda en YouTube. Por favor inténtalo de nuevo."
+      text: "❌ Se produjo un error al realizar la búsqueda en YouTube. Inténtalo de nuevo."
     }, { quoted: m });
   }
-}
-break;
-export default handler
+};
+
+handler.help = ['ytbuscar'];
+handler.tags = ['buscadores'];
+handler.command = /^ytbuscar|playlist|yts(earch)?$/i;
+handler.limit = 1;
+handler.level = 3;
+
+export default handler;
