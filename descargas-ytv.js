a58772639660761f84415372033d13af9e11b537
ManuelLove <104648635+ManuelLove@users.noreply.github.com>
2025-03-21 13:59:01 -0500

    Add files via upload

--- descargas-ytv.js
+++ descargas-ytv.js
@@ -1,43 +1,25 @@
-
-import { ytmp4 } from "@hiudyy/ytdl";  // Se agrega la función para MP4
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 
 var handler = async (m, { conn, args, usedPrefix, command }) => {
 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de YouTube*\n\nEjemplo, !${command} https://youtu.be/85xI8WFMIUY`, m, fake, )
-m.react(rwait)
+if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de YouTube*
 
-await conn.reply(m.chat, `⏰ Espere un momento`, m, fake, )
+Ejemplo, !${command} https://youtu.be/85xI8WFMIUY`, m)
 
-try {
-
-let qu = args[1] || '360'
-let q = qu + 'p'
-let v = args[0]
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
-const dl_url = await yt.video[q].download()
-const ttl = await yt.title
-const size = await yt.video[q].fileSizeH
-m.react(done)
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*Título*\n${ttl}\n\n*Peso*\n${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
-} catch {
+await conn.reply(m.chat, `⏰ Espere un momento`, m)
 
 try {
+    let apiUrl = `https://api.siputzx.my.id/api/d/ytmp4?url=${args[0]}`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    let videoUrl = data.result.url;
 
-let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`)    
-let lolh = await lolhuman.json()
-let n = lolh.result.title || 'error'
-let n2 = lolh.result.link
-let n3 = lolh.result.size
-let n4 = lolh.result.thumbnail
-m.react(done)
-await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `*Título*\n${n}\n\n*Peso*\n${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
-} catch {
-await conn.reply(m.chat, `🚩 *Ocurrió un error*`, m, fake, )
-m.react(error)}}
+    await conn.sendMessage(m.chat, { video: { url: videoUrl }, fileName: `${data.result.title}.mp4`, mimetype: 'video/mp4', caption: `*Título*
+${data.result.title}` }, { quoted: m });
+} catch (error) {
+    await conn.reply(m.chat, `🚩 *Ocurrió un error al descargar el video*`, m);
+}}
 
-}
 handler.help = ['ytv']
 handler.tags = ['descargas']
 handler.command = /^fgmp4|dlmp4|getvid|yt(v|mp4)?$/i
